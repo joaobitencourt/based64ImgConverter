080b47e5aa7715654cd2aff025a0cf9354282dd6
John Vitor <jobitencourt924@gmail.com>
2023-07-31 18:18:36 -0300

    normal funtion working

--- script.js
+++ script.js
@@ -14,8 +14,17 @@ function getImage(){
             let imgBase64 = archiveLoaded.target.result;
             console.log(imgBase64);
             document.querySelector("#preView").src = imgBase64;
+            document.querySelector("imagemString").value = imgBase64;
         }
         // le o conteudo 
         readArchive.readAsDataURL(loadImg);
     }
+}
+
+function copyBase64ImgString() {
+    let stringCopyed  = document.querySelector("#preView");
+    stringCopyed.select();
+    stringCopyed.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Texto copiado para área de transferência! Ctrl+V em algum local para colar");
 }
